219b6c8b72dddf13ca0801408678bc30bb67677f
Rodrigo Viturino <rodrigo.souza@spread.com.br>
2021-04-23 18:23:32 -0300

    Ajustando método DELETE que busca pelo id da rota,valida se retornou e faz a exclusão com splice

--- src/index.js
+++ src/index.js
@@ -48,8 +48,8 @@ app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
-
-  if (repositoryIndex > 0) {
+  console.log(repositoryIndex);
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
