9b41c0dbc6b0d5426798517a77671defc1e114b6
Rodrigo Viturino <rodrigo.souza@spread.com.br>
2021-04-23 18:10:04 -0300

    Ajustando método PUT, aplicando melhorias na busca do repositorio via id com find, validando o repositorio e setando os valores de acordo com informações do request.body

--- src/index.js
+++ src/index.js
@@ -22,25 +22,24 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: 0
   };
-
   repositories.push(repository)
 
-  return response.json(repository);
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const repository = repositories.find(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (!repository) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
-  repositories[repositoryIndex] = repository;
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
 
   return response.json(repository);
 });
